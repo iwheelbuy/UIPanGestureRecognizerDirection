e0d08175101b4eccd7adf252b925424f9e328aed
Mikhail Vasilev <iwheelbuy@gmail.com>
2017-10-21 12:15:23 +0700

    +

---
+++ UIPanGestureRecognizerDirection/Classes/Holder.swift
@@ -0,0 +1,8 @@
+//
+//  Holder.swift
+//  UIPanGestureRecognizerDirection
+//
+//  Created by Mikhail Vasilev on 21.10.17.
+//
+
+import Foundation
---
+++ UIPanGestureRecognizerDirection/Classes/Source.swift
@@ -0,0 +1,8 @@
+//
+//  Source.swift
+//  UIPanGestureRecognizerDirection
+//
+//  Created by Mikhail Vasilev on 21.10.17.
+//
+
+import Foundation
