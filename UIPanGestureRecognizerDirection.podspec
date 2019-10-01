9b3914ef08ef4bc59a5e9324cc0e0bb88684392c
Mikhail Vasilev <iwheelbuy@gmail.com>
2019-10-01 09:54:50 +0300

    1.1.0

--- UIPanGestureRecognizerDirection.podspec
+++ UIPanGestureRecognizerDirection.podspec
@@ -1,5 +1,5 @@
-# git tag 1.0.2
-# git push origin 1.0.2
+# git tag 1.1.0
+# git push origin 1.1.0
 # pod lib lint UIPanGestureRecognizerDirection.podspec --no-clean --verbose
 # pod spec lint UIPanGestureRecognizerDirection.podspec --allow-warnings
 # pod trunk push UIPanGestureRecognizerDirection.podspec --verbose
@@ -7,15 +7,14 @@
 Pod::Spec.new do |s|
 
 s.name                  = 'UIPanGestureRecognizerDirection'
-s.version               = '1.0.2'
-s.ios.deployment_target = '8.0'
+s.version               = '1.1.0'
+s.ios.deployment_target = '11.0'
 s.source_files          = 'UIPanGestureRecognizerDirection/Classes/**/*'
 s.homepage              = 'https://github.com/iwheelbuy/UIPanGestureRecognizerDirection'
 s.license               = 'MIT'
 s.author                = { 'iwheelbuy' => 'iwheelbuy@gmail.com' }
 s.source                = { :git => 'https://github.com/iwheelbuy/UIPanGestureRecognizerDirection.git', :tag => s.version.to_s }
 s.summary               = 'UIPanGestureRecognizerDirection'
-s.cocoapods_version     = '>= 1.4.0'
-s.pod_target_xcconfig   = { 'SWIFT_VERSION' => '4.1' }
+s.cocoapods_version     = '>= 1.8.1'
 
 end
--- UIPanGestureRecognizerDirection/Classes/Source.swift
+++ UIPanGestureRecognizerDirection/Classes/Source.swift
@@ -3,23 +3,17 @@ import UIKit.UIGestureRecognizerSubclass
 extension UIApplication {
     
     override open var next: UIResponder? {
-        DispatchQueue.once {
-            func replace(_ method: Selector, with anotherMethod: Selector, for clаss: AnyClass) {
-                let original = class_getInstanceMethod(clаss, method)
-                let swizzled = class_getInstanceMethod(clаss, anotherMethod)
-                switch class_addMethod(clаss, method, method_getImplementation(swizzled!), method_getTypeEncoding(swizzled!)) {
-                case true:
-                    class_replaceMethod(clаss, anotherMethod, method_getImplementation(original!), method_getTypeEncoding(original!))
-                case false:
-                    method_exchangeImplementations(original!, swizzled!)
-                }
-            }
+        guard let mutex = Once.mutex else {
+            return super.next
+        }
+        mutex.sync {
+            Once.mutex = nil
             let selector1 = #selector(UIPanGestureRecognizer.touchesBegan(_:with:))
             let selector2 = #selector(UIPanGestureRecognizer.swizzling_touchesBegan(_:with:))
-            replace(selector1, with: selector2, for: UIPanGestureRecognizer.self)
+            replace(UIPanGestureRecognizer.self, selectorOriginal: selector1, selectorSwizzled: selector2)
             let selector3 = #selector(UIPanGestureRecognizer.touchesMoved(_:with:))
             let selector4 = #selector(UIPanGestureRecognizer.swizzling_touchesMoved(_:with:))
-            replace(selector3, with: selector4, for: UIPanGestureRecognizer.self)
+            replace(UIPanGestureRecognizer.self, selectorOriginal: selector3, selectorSwizzled: selector4)
         }
         return super.next
     }
@@ -29,96 +23,196 @@ extension UIPanGestureRecognizer {
     
     @objc func swizzling_touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
         self.swizzling_touchesBegan(touches, with: event)
-        guard direction != .all else { return }
-        touchesBegan = true
+        guard self.direction != .all else {
+            return
+        }
+        self.touchesBegan = true
     }
     
     @objc func swizzling_touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
         self.swizzling_touchesMoved(touches, with: event)
-        guard touchesBegan == true else { return }
+        guard self.touchesBegan == true else {
+            return
+        }
         let directions = touches
-            .map({ $0.location(in: $0.view) - $0.previousLocation(in: $0.view) })
-            .map({ $0.direction })
-        guard directions.filter({ $0 != .none }).count > 0 else { return }
+            .map({ (touch: UITouch) -> UIPanGestureRecognizer.Direction in
+                let locationCurrent = touch.location(in: touch.view)
+                let locationPrevious = touch.previousLocation(in: touch.view)
+                return UIPanGestureRecognizer.Direction(locationCurrent: locationCurrent, locationPrevious: locationPrevious)
+            })
+        guard directions.filter({ $0 != .none }).count > 0 else {
+            return
+        }
         defer {
             touchesBegan = false
         }
-        guard directions.filter({ direction.contains($0) == false }).count > 0 else { return }
+        guard directions.filter({ self.direction.contains($0) == false }).count > 0 else {
+            return
+        }
         state = .failed
     }
 }
 
 public extension UIPanGestureRecognizer {
     
-    public struct Direction: OptionSet {
+    struct Direction: OptionSet {
         
         public let rawValue: Int
         
         public init(rawValue: Int) {
             self.rawValue = rawValue
         }
-        
-        public static let none  = Direction(rawValue: 1 << 0)
-        public static let up    = Direction(rawValue: 1 << 1)
-        public static let down  = Direction(rawValue: 1 << 2)
-        public static let left  = Direction(rawValue: 1 << 3)
-        public static let right = Direction(rawValue: 1 << 4)
-        
+
         public static let all: Direction = [.up, .down, .left, .right]
+        public static let down = Direction(rawValue: 1 << 2)
         public static let horizontal: Direction = [.left, .right]
+        public static let left = Direction(rawValue: 1 << 3)
+        public static let none = Direction(rawValue: 1 << 0)
+        public static let right = Direction(rawValue: 1 << 4)
+        public static let up = Direction(rawValue: 1 << 1)
         public static let vertical: Direction = [.up, .down]
+
+        public init(locationCurrent: CGPoint, locationPrevious: CGPoint) {
+            let vector = CGPoint(
+                x: locationCurrent.x - locationPrevious.x,
+                y: locationCurrent.y - locationPrevious.x
+            )
+            self.init(vector: vector)
+        }
+
+        public init(vector: CGPoint) {
+            switch vector {
+            case .zero:
+                self = UIPanGestureRecognizer.Direction.none
+            default:
+                switch fabs(vector.x) > fabs(vector.y) {
+                case true:
+                    switch vector.x > 0 {
+                    case true:
+                        self = UIPanGestureRecognizer.Direction.right
+                    case false:
+                        self = UIPanGestureRecognizer.Direction.left
+                    }
+                case false:
+                    switch vector.y > 0 {
+                    case true:
+                        self = UIPanGestureRecognizer.Direction.down
+                    case false:
+                        self = UIPanGestureRecognizer.Direction.up
+                    }
+                }
+            }
+        }
     }
 
-    public var currentDirection: UIPanGestureRecognizer.Direction {
+    var directionCurrent: UIPanGestureRecognizer.Direction {
         get {
-            return velocity(in: view).direction
+            let vector = self.velocity(in: self.view)
+            return UIPanGestureRecognizer.Direction(vector: vector)
         }
     }
     
-    public var direction: UIPanGestureRecognizer.Direction {
+    var direction: UIPanGestureRecognizer.Direction {
         get {
-            return holder.direction
+            return self.holder.direction
         }
         set {
-            holder.direction = newValue
+            self.holder.direction = newValue
         }
     }
     
-    var touchesBegan: Bool {
+    private var touchesBegan: Bool {
         get {
-            return holder.touchesBegan
+            return self.holder.touchesBegan
         }
         set {
-            holder.touchesBegan = newValue
+            self.holder.touchesBegan = newValue
         }
     }
 }
 
-extension CGPoint {
-    
-    var direction: UIPanGestureRecognizer.Direction {
-        if self == .zero {
-            return UIPanGestureRecognizer.Direction.none
-        }
-        switch fabs(x) > fabs(y) {
-        case true:
-            switch x > 0 {
-            case true:
-                return UIPanGestureRecognizer.Direction.right
-            case false:
-                return UIPanGestureRecognizer.Direction.left
-            }
-        case false:
-            switch y > 0 {
-            case true:
-                return UIPanGestureRecognizer.Direction.down
-            case false:
-                return UIPanGestureRecognizer.Direction.up
-            }
+// MARK: - Holder
+
+fileprivate extension UIPanGestureRecognizer {
+
+    var holder: Holder {
+        if let holder = objc_getAssociatedObject(self, &UIPANGESTURERECOGNIZER_FRAMEWORK_HOLDER_ASSOCIATED_OBJECT_KEY) as? Holder {
+            return holder
+        } else {
+            let holder = Holder()
+            let policy = objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC
+            objc_setAssociatedObject(self, &UIPANGESTURERECOGNIZER_FRAMEWORK_HOLDER_ASSOCIATED_OBJECT_KEY, holder, policy)
+            return holder
         }
     }
-    
-    static func -(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
-        return CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+}
+
+fileprivate var UIPANGESTURERECOGNIZER_FRAMEWORK_HOLDER_ASSOCIATED_OBJECT_KEY: UInt8 = 0
+
+fileprivate final class Holder {
+
+    var direction: UIPanGestureRecognizer.Direction = .all
+    var touchesBegan: Bool = false
+}
+
+// MARK: - Mutex
+
+fileprivate final class Mutex {
+
+    private var mutex = pthread_mutex_t()
+
+    init?(recursive: Bool = false) {
+        var attributes = pthread_mutexattr_t()
+        guard pthread_mutexattr_init(&attributes) == 0 else {
+            return nil
+        }
+        pthread_mutexattr_settype(&attributes, recursive ? PTHREAD_MUTEX_RECURSIVE : PTHREAD_MUTEX_NORMAL)
+        guard pthread_mutex_init(&mutex, &attributes) == 0 else {
+            return nil
+        }
+    }
+
+    deinit {
+        pthread_mutex_destroy(&mutex)
+    }
+
+    private func lock() {
+        pthread_mutex_lock(&mutex)
+    }
+
+    private func unlock() {
+        pthread_mutex_unlock(&mutex)
+    }
+
+    func sync<R>(block: () throws -> R) rethrows -> R {
+        lock()
+        defer {
+            unlock()
+        }
+        return try block()
+    }
+}
+
+// MARK: - Once
+
+fileprivate final class Once {
+
+    static var mutex: Mutex? = Mutex()
+}
+
+// MARK: - Replace
+
+fileprivate func replace(_ anyClass: AnyClass, selectorOriginal: Selector, selectorSwizzled: Selector) {
+    guard let original = class_getInstanceMethod(anyClass, selectorOriginal) else {
+        return
+    }
+    guard let swizzled = class_getInstanceMethod(anyClass, selectorSwizzled) else {
+        return
+    }
+    switch class_addMethod(anyClass, selectorOriginal, method_getImplementation(swizzled), method_getTypeEncoding(swizzled)) {
+    case true:
+        class_replaceMethod(anyClass, selectorSwizzled, method_getImplementation(original), method_getTypeEncoding(original))
+    case false:
+        method_exchangeImplementations(original, swizzled)
     }
 }
